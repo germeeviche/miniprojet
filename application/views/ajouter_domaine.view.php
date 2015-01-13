33e87915c886b4233f4bf737baf33ae9c037a02a
GermaineChrisella <germainembembe61@gmail.com>
2015-01-13 17:42:54 +0100

    Update ajouter_domaine.view.php

--- application/views/ajouter_domaine.view.php
+++ application/views/ajouter_domaine.view.php
@@ -1,8 +1,34 @@
-<?php
+<form class="form-horizontal" method="POST">
+<fieldset>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!-- Form Name -->
+<legend>DOMAINE</legend>
 
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="textinput">LIBDOMAINE</label>
+  <div class="controls">
+    <input id="textinput" name="textinput" placeholder="entrer le libelle du domaine" class="input-xlarge" type="text">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="textinput">MATRICULE</label>
+  <div class="controls">
+    <input id="textinput" name="textinput" placeholder="inserer le matricule" class="input-xlarge" type="text">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="control-group">
+  <label class="control-label" for="singlebutton"></label>
+  <div class="controls">
+    <button id="singlebutton" name="singlebutton" class="btn btn-primary">AJOUTER</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
